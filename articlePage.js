cfaa3239b3dc82c31f087e1562a7a70abe8a445d
João Soares <joao.soares@cwi.com.br>
2023-03-06 09:49:39 -0300

    v3.3

--- articlePage.js
+++ articlePage.js
@@ -1,16 +1,15 @@
 import { generateArticle } from "./script.js";
 import { articles } from "./articles.js";
 
-const article = document.getElementById("article");
-const currentPath = window.location.pathname;
-
-let articlePage;
-if (currentPath === "/summer.html") {
-  articlePage = articles.summer;
-} else {
-  articlePage = articles.winter;
-}
-
 window.addEventListener("DOMContentLoaded", function () {
+  const article = document.getElementById("article");
+  const currentPath = window.location.href;
+  let articlePage;
+
+  if (currentPath.includes("summer")) {
+    articlePage = articles.summer;
+  } else {
+    articlePage = articles.winter;
+  }
   generateArticle(article, articlePage);
 });
